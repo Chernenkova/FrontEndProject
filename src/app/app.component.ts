f2152a705fc3ace2c33d51031f6e3d8d3f71d7dc
Chernenkova <elena.chernenkova@bk.ru>
2018-01-30 19:54:05 +0300

    Frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-my',
+  template: `<div>
+    <nav class="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header navbar-left">
+          <div *ngIf="try(); else unset">
+            <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-user"> Hello, {{userName.trim()}}!  {{raiting}}</span></a>
+            <button type="button" class="btn btn-default" (click)="sett()"><span class="glyphicon glyphicon-cog"></span> Settings</button>
+            <button type="button" class="btn btn-default" (click)="signOut()">Sign out</button>
+          </div>
+          <ng-template #unset>
+            <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-user"> Hello!</span></a>
+            <button type="button" class="btn btn-default" (click)="wantToSignIn()">Sign in</button>
+            <button type="button" class="btn btn-default" (click)="wantToSignUp()">Sign up</button>
+          </ng-template>
+
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav nav-stacked">
+
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+                    <router-outlet></router-outlet>
+               </div>`,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  loginIn: boolean;
+  userName: string = null;
+  raiting: string = null;
+
+   constructor(private router: Router, @Inject(DOCUMENT) private document: any) {}
+  ngOnInit(): void {
+    this.userName = localStorage.getItem('name');
+    this.raiting = localStorage.getItem('raiting');
+  }
+  try(): boolean {
+    if(localStorage.getItem('name') === null)
+      return false;
+    this.userName = localStorage.getItem('name');
+    this.loginIn = this.userName.length !== 0;
+    this.raiting = localStorage.getItem('raiting');
+    return this.loginIn;
+  }
+
+  signOut() {
+    this.loginIn = false;
+    this.userName = null;
+    this.raiting = null;
+    localStorage.clear();
+    this.try();
+    this.document.location.href = '';
+  }
+  wantToSignIn() {
+    this.document.location.href = '';
+  }
+  wantToSignUp() {
+    this.document.location.href = '/signUp';
+  }
+  sett() {
+    this.document.location.href = '/settings';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,142 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {CardComponent} from './tasks/cards/card.component';
+import {CreatingComponent} from './creatingtasks/creating.component';
+import {QuestionViewComponent} from './questions/question_view_component';
+import {CreatingQuestionComponent} from './questions/creating_question_component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {DialogConfirmComponent, DictionaryComponent} from './tasks/dictionary_component';
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,
+} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CdkTableModule} from '@angular/cdk/table';
+import {CabinetComponent} from './welcome/cabinet.component';
+import {SettingsComponent} from './settings/settings.component';
+import {UpdateUserComponent} from './settings/update.user.component';
+import {TaskListComponent} from './welcome/task_list_component';
+import {AdminHelloComponent} from "./admin/admin.hello.component";
+import {AddWordComponent} from "./admin/add.word.component";
+import {AdminAddNewGroupComponent} from "./admin/admin.add.new.group.component";
+import {DialogPasswordWarningComponent, RecoverPasswordComponent} from "./settings/recover.password.component";
+import {RequestRecoverPasswordComponent} from "./settings/request.recover.password.component";
+import {WelcomeSignUpComponent} from "./welcome/welcome.sign.up.component";
+
+
+const appRoutes: Routes = [
+  { path: '', component: WelcomeComponent},
+  { path: 'questions', component: CreatingQuestionComponent},
+  { path: 'view', component: QuestionViewComponent },
+  { path: 'card', component: CardComponent},
+  { path: 'dictionary', component: DictionaryComponent},
+  { path: 'cabinet', component: CabinetComponent},
+  { path: 'signUp', component: WelcomeSignUpComponent},
+  { path: 'settings', component: SettingsComponent},
+  { path: 'updateUser', component: UpdateUserComponent},
+  { path: 'admin', component: AdminHelloComponent},
+  { path: 'admin/addNewWords', component: AddWordComponent},
+  { path: 'admin/addNewGroups', component: AdminAddNewGroupComponent},
+  { path: 'recover-request', component: RequestRecoverPasswordComponent},
+  { path: 'recover/:id', component: RecoverPasswordComponent},
+  { path: '**', redirectTo: '/'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    CardComponent,
+    CreatingComponent,
+    QuestionViewComponent,
+    CreatingQuestionComponent,
+    DictionaryComponent,
+    DialogConfirmComponent,
+    CabinetComponent,
+    SettingsComponent,
+    UpdateUserComponent,
+    TaskListComponent,
+    AdminHelloComponent,
+    AddWordComponent,
+    AdminAddNewGroupComponent,
+    RecoverPasswordComponent,
+    RequestRecoverPasswordComponent,
+    DialogPasswordWarningComponent,
+    WelcomeSignUpComponent
+  ],
+  entryComponents: [
+    DialogConfirmComponent,
+    DictionaryComponent,
+    DialogPasswordWarningComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot(appRoutes),
+    MatTableModule, MatPaginatorModule, MatCheckboxModule, BrowserAnimationsModule,
+    CdkTableModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
